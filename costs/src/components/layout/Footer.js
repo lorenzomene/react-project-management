7eb7b3c1cab82bcbe8f1c5764785ed09c43b4b16
lorenzomene <lorenzospmene@gmail.com>
2023-03-08 11:23:49 -0300

    navbar footer router

---
+++ costs/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import {FaLinkedin, FaGithub} from 'react-icons/fa';
+
+import styles from './Footer.module.css';
+
+function Footer (){
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>  
+                <li><FaGithub /></li>    
+                <li><FaLinkedin /> </li>    
+            </ul>    
+            <p className={styles.copyright}><span>Costs</span> &copy; 2023</p>
+        </footer>
+    )
+}
+
+export default Footer;
